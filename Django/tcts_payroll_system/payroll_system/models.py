cc9d8cee1107a8e2d10ca4102aa984527ae77c42
Ejay <ejayrulona@gmail.com>
2025-02-19 11:07:35 -0800

    updated models

--- Django/tcts_payroll_system/payroll_system/models.py
+++ Django/tcts_payroll_system/payroll_system/models.py
@@ -4,9 +4,7 @@ from django.db import models
 
 class Admin(models.Model):
     admin_id = models.AutoField(primary_key=True)
-    first_name = models.CharField(max_length=100, null=False)
-    last_name = models.CharField(max_length=100, null=False)
-    email = models.EmailField(unique=True, null=False)
+    username = models.CharField(max_length=100, null=False)
     password = models.CharField(max_length=100, null=False)
 
 class Employee(models.Model):
