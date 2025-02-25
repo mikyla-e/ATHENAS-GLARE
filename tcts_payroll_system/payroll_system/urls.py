5d412ed4440a77afb407264927e5852351a52de7
Ejay <ejayrulona@gmail.com>
2025-02-25 10:30:19 -0800

    Updated project

---
+++ tcts_payroll_system/payroll_system/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+app_name = 'payroll_system'
+
+urlpatterns = [
+    path('dashboard/', views.dashboard, name='dashboard'), 
+    path('time_in_out/', views.time_in_out, name='time_in_out'),
+]
