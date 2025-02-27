8c9409b6fd764dfb71b67088d9358b084f5cf63f
Ejay <ejayrulona@gmail.com>
2025-02-27 10:42:37 -0800

    added initial employee_profile view function

--- Django/tcts_payroll_system/payroll_system/urls.py
+++ Django/tcts_payroll_system/payroll_system/urls.py
@@ -5,9 +5,10 @@ from . import views
 app_name = 'payroll_system'
 
 urlpatterns = [
-    path('dashboard/', views.dashboard, name='dashboard'), 
     path('time_in_out/', views.time_in_out, name='time_in_out'),
+    path('dashboard/', views.dashboard, name='dashboard'), 
     path('employee_registration/', views.employee_registration, name='employee_registration'),
-    path('payroll/', views.payrolls, name='payrolls'),
-    path('employees/', views.employees, name='employees')
+    path('employees/', views.employees, name='employees'),
+    path('employee_profile/<int:employee_id>/', views.employee_profile, name='employee_profile'),
+    path('payrolls/', views.payrolls, name='payrolls'),
 ]
--- Django/tcts_payroll_system/payroll_system/views.py
+++ Django/tcts_payroll_system/payroll_system/views.py
@@ -8,14 +8,11 @@ from .models import Employee
 # Create your views here.
 
 @login_required
-def dashboard(request):
-    return render(request, 'payroll_system/dashboard.html')
-
-def payrolls(request):
-    return render(request, 'payroll_system/payroll.html')
-
 def time_in_out(request):
     return render(request, 'payroll_system/time_in_out.html')
+
+def dashboard(request):
+    return render(request, 'payroll_system/dashboard.html')
 
 def employee_registration(request):
     if request.method == "POST":
@@ -35,3 +32,12 @@ def employees(request):
     }
     return render(request, 'payroll_system/employees.html', context)
 
+def employee_profile(request, employee_id):
+    employee = Employee.objects.get(employee_id=employee_id)
+    context = {
+        'employee' : employee,
+    }
+    return render(request, 'payroll_system/employee_profile.html', context)
+
+def payrolls(request):
+    return render(request, 'payroll_system/payroll.html')
