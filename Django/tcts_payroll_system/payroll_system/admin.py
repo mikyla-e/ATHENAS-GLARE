8a10b7aa39a85fcf72fa49248f5200d2ac650f17
Ejay <ejayrulona@gmail.com>
2025-04-27 12:11:53 +0800

    added validation for login, admin edit, & change password

--- Django/tcts_payroll_system/payroll_system/admin.py
+++ Django/tcts_payroll_system/payroll_system/admin.py
@@ -1,17 +1,8 @@
 from django.contrib import admin
 from .models import Employee, Payroll, Attendance, History, Service, Customer, Vehicle, Task
 
-class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ('employee_id', 'first_name', 'middle_name', 'last_name', 'gender', 'date_of_birth', 'contact_number', 'emergency_contact',
-                     'region', 'province', 'city', 'barangay', 'highest_education', 'work_experience', 'date_of_employment', 'days_worked', 'employee_status', 'absences', 
-                     'employee_image')
-
-class PayrollAdmin(admin.ModelAdmin):
-    list_display = ('payroll_id', 'rate', 'incentives', 'payroll_status', 'deductions', 'salary', 'cash_advance', 'under_time',
-                     'payment_date')    
-
-admin.site.register(Employee, EmployeeAdmin)
-admin.site.register(Payroll, PayrollAdmin)
+admin.site.register(Employee)
+admin.site.register(Payroll)
 admin.site.register(Attendance)
 admin.site.register(History)
 admin.site.register(Service)
--- Django/tcts_payroll_system/payroll_system/forms.py
+++ Django/tcts_payroll_system/payroll_system/forms.py
@@ -583,7 +583,6 @@ class VehicleForm(forms.ModelForm):
                 raise forms.ValidationError("This plate number is already registered.")
 
         return plate_number
-
     
     def clean(self):
         cleaned_data = super().clean()
@@ -595,16 +594,16 @@ class VehicleForm(forms.ModelForm):
         return cleaned_data
     
 class AdminEditProfileForm(UserChangeForm):
-    username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
+    username = forms.CharField(max_length=100, label='username', widget=forms.TextInput(attrs={
             'class': 'w-full bg-white px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500',
             'placeholder': 'Enter username'}))
-    first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
+    first_name = forms.CharField(max_length=100, label='first name', widget=forms.TextInput(attrs={
             'class': 'w-full bg-white px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500',
             'placeholder': 'Enter First Name'}))
-    last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
+    last_name = forms.CharField(max_length=100, label='last name', widget=forms.TextInput(attrs={
             'class': 'w-full bg-white px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500',
             'placeholder': 'Enter Last Name'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={
+    email = forms.EmailField(label='email', widget=forms.EmailInput(attrs={
             'class': 'w-full bg-white px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500',
             'placeholder': 'Enter Email'}))
     
