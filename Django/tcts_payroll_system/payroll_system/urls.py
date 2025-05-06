49a9cf0a8acee8738c04817b4cf8202ae67ef81f
Creoum <floresaaron0821@gmail.com>
2025-05-06 14:18:17 +0800

    Donut Chart

--- Django/tcts_payroll_system/payroll_system/urls.py
+++ Django/tcts_payroll_system/payroll_system/urls.py
@@ -36,8 +36,9 @@ urlpatterns = [
     path('ajax/get-barangays/', views.get_barangays, name='get_barangays'),
     path('ajax/customer/<int:customer_id>/', views.get_customer_details, name='get_customer_details'),
     # path('payroll/update-payday/', views.update_payday, name='update_payday'),
-    path('api/attendance-stats/', views.get_attendance_stats, name='attendance_stats'),
     path('api/payroll-by-week/', views.payroll_by_week, name='payroll_by_week'),
     path('payslip/', views.payslip, name='payslip'),
     path('api/payroll-stats/', views.payroll_stats_api, name='payroll_stats_api'),
+    #new - flores
+    path('attendance/summary/', views.attendance_summary, name='attendance-summary'),
 ]
